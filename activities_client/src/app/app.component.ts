eafaa221c65ec2bba34c3c46452a7812b1d4923d
Maicol Orejuela <viafara96@hotmail.com>
2022-09-07 22:24:11 -0500

    Start project from angular

--- activities_client/src/app/app.component.ts
+++ activities_client/src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'activities_client';
+export class AppComponent implements OnInit{
+  
+  tasks: any[] = [];
+  task = 
+  {
+    id: null,
+    name: '',
+    complete: false
+  }
+
+  constructor
+  (
+    private appService: AppService
+  ){}
+
+  ngOnInit(): void 
+  {
+    this.getAll()
+  }
+
+  getAll()
+  {
+    this.appService.getAll()
+    .subscribe((data: any) => this.tasks = data);
+  }
+
+  save()
+  {
+    if(this.task.id)
+    {
+      this.appService.update(this.task.id!, this.task)
+      .subscribe(()=> this.getAll());
+    }
+    else
+    {
+      this.appService.create(this.task)
+      .subscribe(() => this.getAll());
+    }
+
+    this.task = 
+    {
+      id: null,
+      name: '',
+      complete: false
+    }
+  }
+
+  edit(task: any)
+  {
+    this.task = {...task};
+  }
+  delete(task:any)
+  {
+    this.appService.delete(task.id)
+    .subscribe(()=>this.getAll());
+  }
 }
