713490d02ac53421d76507c25b11233d7f2bee8e
morejuela <maicol.orejuela.asesoftware@gmail.com>
2024-09-18 23:20:56 -0500

    Glassfish and jersy client is added to security

--- WebServices/AMS_SGA_web-REST/src/main/java/services/impl/PersonServiceImpl.java
+++ WebServices/AMS_SGA_web-REST/src/main/java/services/impl/PersonServiceImpl.java
@@ -8,7 +8,12 @@ import jakarta.ejb.Stateless;
 import java.util.List;
 import jakarta.inject.Inject;
 
+import jakarta.annotation.security.DeclareRoles;
+import jakarta.annotation.security.RolesAllowed;
+
 @Stateless
+@DeclareRoles({"ROLE_ADMIN","ROLE_USER"})
+@RolesAllowed({"ROLE_ADMIN","ROLE_USER"})
 public class PersonServiceImpl implements PersonService {
 
     @Inject
@@ -36,6 +41,7 @@ public class PersonServiceImpl implements PersonService {
     }
 
     @Override
+    @RolesAllowed("ROLE_ADMIN")
     public void deletePerson(Person person) {
        personDao.delete(person);
     }
