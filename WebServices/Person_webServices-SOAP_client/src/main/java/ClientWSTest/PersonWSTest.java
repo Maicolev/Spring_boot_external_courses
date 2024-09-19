acf327def3b4dc0a9d5281ce3c10daf0da90ffba
morejuela <maicol.orejuela.asesoftware@gmail.com>
2024-09-18 23:48:17 -0500

    Security and rolls is added

--- WebServices/Person_webServices-SOAP_client/src/main/java/ClientWSTest/PersonWSTest.java
+++ WebServices/Person_webServices-SOAP_client/src/main/java/ClientWSTest/PersonWSTest.java
@@ -3,10 +3,18 @@ package ClientWSTest;
 import clientWS.service.Person;
 import clientWS.service.PersonServiceImplService;
 import clientWS.service.PersonServiceWs;
+import jakarta.xml.ws.BindingProvider;
+
+import java.util.List;
 
 public class PersonWSTest {
    public static void main(String[] args) {
        PersonServiceWs personServiceWS = new PersonServiceImplService().getPersonServiceImplPort();
+
+       ((BindingProvider)personServiceWS).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, "admin");
+       ((BindingProvider)personServiceWS).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, "admin");
+
+       System.out.println("list method on ws");
        for (Person person : personServiceWS.getAllPersons()) {
            System.out.println(person.getFirstname() + " " + person.getLastname());
        }
